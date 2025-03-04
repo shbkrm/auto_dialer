d487f4fd71cbdc9ea4f6196f8a79e54d60685ae0
Shuaib Akram <sheikshuaibakram@gmail.com>
2025-03-04 14:04:20 +0530

    Initial Frame

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# auto_dialer
+# Auto Dialer for ERPNext
+This app integrates with Tata Smartflo to provide an auto dialer for ERPNext CRM.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="auto_dialer",
+    version="0.0.1",
+    description="Auto Dialer for ERPNext using Tata Smartflo",
+    author="Your Name",
+    author_email="your_email@example.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
